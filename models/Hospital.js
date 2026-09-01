3ff67d9004bd96b21a04c4d5dcf8756dd9e35c9b
mohamedsallam27 <83533876+mohamedsallam27@users.noreply.github.com>
2026-09-01 21:47:07 +0300

    Update Hospital.js

--- models/Hospital.js
+++ models/Hospital.js
@@ -2,10 +2,24 @@ const mongoose = require('mongoose');
 
 const HospitalSchema = new mongoose.Schema({
   hospitalName: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  location: { type: String, required: true },
   specialtyNeeded: { type: String, required: true },
-  financialOffer: { type: String, required: true },
-  benefits: { type: String, default: '' },
   qualification: { type: String, required: true },
+  benefits: { type: String, default: '' },
+  financialOffer: { type: String, required: true },
+  salaryByQualification: {
+    collegeGrad: { type: String, default: '' },
+    instituteGrad: { type: String, default: '' },
+    collegeIntern: { type: String, default: '' },
+    instituteIntern: { type: String, default: '' },
+    collegeStudent: { type: String, default: '' },
+    instituteStudent: { type: String, default: '' }
+  },
+  contactName: { type: String, required: true },
+  contactJobTitle: { type: String, required: true },
+  contactPhone: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
